16c6c829cc0de5ae1132e9b8dac2c4451897e50b
Danila <novash.danila@yandex.by>
2025-07-26 10:30:30 +0300

    feat(routing): implement test navigation flow

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -12,22 +12,54 @@ import {
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
 import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
+  function handleModalClose(): void {
+    throw new Error('Function not implemented.');
+  }
+
   return (
     <div className={styles.app}>
       <AppHeader />
       <Routes>
         <Route path='/' element={<ConstructorPage />} />
-        <Route path='/feed' element={<Feed />} />
+        <Route path='/feed' element={<Feed />}>
+          <Route
+            path=':number'
+            element={
+              <Modal title='Детали заказа' onClose={handleModalClose}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+        </Route>
+        <Route
+          path='/ingredients/:id'
+          element={
+            <Modal title='Детали ингредиента' onClose={handleModalClose}>
+              <IngredientDetails />
+            </Modal>
+          }
+        />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
         <Route path='/reset-password' element={<ResetPassword />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/profile/orders' element={<ProfileOrders />} />
+        <Route path='/profile'>
+          <Route index element={<Profile />} />
+          <Route path='orders' element={<ProfileOrders />}>
+            <Route
+              path=':number'
+              element={
+                <Modal title='Детали заказа' onClose={handleModalClose}>
+                  <OrderInfo />
+                </Modal>
+              }
+            />
+          </Route>
+        </Route>
         <Route path='/*' element={<NotFound404 />} />
       </Routes>
     </div>
