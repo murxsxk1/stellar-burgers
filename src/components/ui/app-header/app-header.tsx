6b5ec7d966ed6bc3e2e07a38746b7de434539a0b
Danila <novash.danila@yandex.by>
2025-07-27 12:34:48 +0300

    feat(routing): set up header navigation panel

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -14,43 +14,64 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
         <NavLink
-          to={'/'}
-          className={({ isActive }) => (isActive ? styles.link_active : '')}
+          to='/'
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.link_active : ''}`
+          }
         >
-          <>
-            <BurgerIcon type={'primary'} />
-            <p className='text text_type_main-default ml-2 mr-10'>
-              Конструктор
-            </p>
-          </>
+          {({ isActive }) => (
+            <>
+              <BurgerIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2 mr-10'>
+                Конструктор
+              </p>
+            </>
+          )}
         </NavLink>
 
-        <>
-          <NavLink
-            to={'/feed'}
-            className={({ isActive }) => (isActive ? styles.link_active : '')}
-          >
-            <ListIcon type={'primary'} />
-            <p className='text text_type_main-default ml-2'>Лента заказов</p>
-          </NavLink>
-        </>
-      </div>
-
-      <div className={styles.logo}>
-        <Logo className='' />
+        <NavLink
+          to='/feed'
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.link_active : ''}`
+          }
+        >
+          {({ isActive }) => (
+            <>
+              <ListIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2'>Лента заказов</p>
+            </>
+          )}
+        </NavLink>
       </div>
 
       <NavLink
-        to={'/profile'}
-        className={({ isActive }) => (isActive ? styles.link_active : '')}
+        to='/'
+        className={({ isActive }) =>
+          `${styles.link} ${isActive ? styles.link_active : ''}`
+        }
       >
-        <div className={styles.link_position_last}>
-          <ProfileIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2'>
-            {userName || 'Личный кабинет'}
-          </p>
+        <div className={styles.logo}>
+          <Logo className='' />
         </div>
       </NavLink>
+
+      <div className={styles.link_position_last}>
+        <NavLink
+          to='/profile'
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.link_active : ''}`
+          }
+        >
+          {({ isActive }) => (
+            <>
+              <ProfileIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2'>
+                {userName || 'Личный кабинет'}
+              </p>
+            </>
+          )}
+        </NavLink>
+      </div>
     </nav>
   </header>
 );
