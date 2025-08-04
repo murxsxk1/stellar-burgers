7f1134db5f8f8c70f249d20946699cb54b9c3230
Danila <novash.danila@yandex.by>
2025-08-04 10:30:30 +0300

    feat(profile): implement profile data editing functionality

--- src/components/ui/pages/profile/type.ts
+++ src/components/ui/pages/profile/type.ts
@@ -6,9 +6,16 @@ export type ProfileUIProps = {
     email: string;
     password: string;
   };
+  passwordDisplayValue: string;
   isFormChanged: boolean;
+  editMode: {
+    name: boolean;
+    email: boolean;
+    password: boolean;
+  };
   handleSubmit: (e: SyntheticEvent) => void;
   handleCancel: (e: SyntheticEvent) => void;
   handleInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleEditToggle: (fieldName: 'name' | 'email' | 'password') => void;
   updateUserError?: string;
 };
--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from '../../services/store';
 import { updateUser } from '../../services/slices/user';
 
 export const Profile: FC = () => {
-  /** TODO: взять переменную из стора */
   const user = useSelector((state) => state.user.data);
   const dispatch = useDispatch();
 
@@ -12,6 +11,19 @@ export const Profile: FC = () => {
     name: user?.name || '',
     email: user?.email || '',
     password: ''
+  });
+
+  const getPasswordDisplayValue = () => {
+    if (editMode.password) {
+      return formValue.password;
+    }
+    return '••••••••';
+  };
+
+  const [editMode, setEditMode] = useState({
+    name: false,
+    email: false,
+    password: false
   });
 
   useEffect(() => {
@@ -38,6 +50,12 @@ export const Profile: FC = () => {
     };
 
     await dispatch(updateUser(updateData));
+
+    setEditMode({
+      name: false,
+      email: false,
+      password: false
+    });
   };
 
   const handleCancel = (e: SyntheticEvent) => {
@@ -47,22 +65,49 @@ export const Profile: FC = () => {
       email: user?.email || '',
       password: ''
     });
+
+    setEditMode({
+      name: false,
+      email: false,
+      password: false
+    });
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormValue((prevState) => ({
+    if (editMode[e.target.name as keyof typeof editMode]) {
+      setFormValue((prevState) => ({
+        ...prevState,
+        [e.target.name]: e.target.value
+      }));
+    }
+  };
+
+  const handleEditToggle = (fieldName: keyof typeof editMode) => {
+    setEditMode((prevState) => ({
       ...prevState,
-      [e.target.name]: e.target.value
+      [fieldName]: !prevState[fieldName]
     }));
+    if (editMode[fieldName]) {
+      if (fieldName === 'name') {
+        setFormValue((prev) => ({ ...prev, name: user?.name || '' }));
+      } else if (fieldName === 'email') {
+        setFormValue((prev) => ({ ...prev, email: user?.email || '' }));
+      } else if (fieldName === 'password') {
+        setFormValue((prev) => ({ ...prev, password: '' }));
+      }
+    }
   };
 
   return (
     <ProfileUI
       formValue={formValue}
+      passwordDisplayValue={getPasswordDisplayValue()}
       isFormChanged={isFormChanged}
+      editMode={editMode}
       handleCancel={handleCancel}
       handleSubmit={handleSubmit}
       handleInputChange={handleInputChange}
+      handleEditToggle={handleEditToggle}
     />
   );
 };
